2ac3f6b7b5940d5d0e448e02234741af48fa6e2a
ptpofficialxd <aekkungzaza@gmail.com>
2024-05-30 07:04:19 +0700

    improve face application

--- Py-Scripts/main.py
+++ Py-Scripts/main.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2a55fbc7f9e7fdf832fc17b971ed78518b230dcd16d1c2fddcc6c45f29f43683
-size 6779
+oid sha256:8a06971a741dce95abe23c334aef61b4e2a72b53629e4cab8e5b5d02a76e82e8
+size 6085
